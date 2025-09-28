d41d204f27ecd9234464b78c9ed57ef640152311
Kristoffer Velazquez <kristoffervelazquez49@live.com>
2025-09-28 06:41:06 -0700

    organize structure

--- app/components/MuiProvider.tsx
+++ app/components/MuiProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
-import { theme } from '../theme'
+import { theme } from './theme'
 
 
 
