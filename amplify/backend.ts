4d55ce667106e90dd151d6113efb33cc7da071ad
Kristoffer Velazquez <kristoffervelazquez49@live.com>
2025-09-29 00:07:30 -0700

    feat: implement CreateEmbedding streaming policy and event source mapping

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,47 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 import { createEmbedding } from './functions/createEmbedding/resource';
+import { Effect, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Stack } from 'aws-cdk-lib';
+import { EventSourceMapping, StartingPosition } from 'aws-cdk-lib/aws-lambda';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
-  createEmbedding
+  createEmbedding,
 });
+
+const itemTable = backend.data.resources.tables["Item"];
+
+const policy = new Policy(
+  Stack.of(itemTable),
+  "CreateEmbeddingStreamingPolicy",
+  {
+    statements: [
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          "dynamodb:DescribeStream",
+          "dynamodb:GetRecords",
+          "dynamodb:GetShardIterator",
+          "dynamodb:ListStreams",
+        ],
+        resources: ["*"],
+      }),
+    ],
+  }
+);
+
+backend.createEmbedding.resources.lambda.role?.attachInlinePolicy(policy);
+
+const mapping = new EventSourceMapping(
+  Stack.of(itemTable),
+  "CreateEmbeddingItemEventStreamMapping",
+  {
+    target: backend.createEmbedding.resources.lambda,
+    eventSourceArn: itemTable.tableStreamArn,
+    startingPosition: StartingPosition.LATEST,
+  }
+);
+
+mapping.node.addDependency(policy);
